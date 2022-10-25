bbbe3c103ecbeb1fc127a1c2cc38f359e0d2b614
Antonio <agarciaexposito.ies16@gmail.com>
2022-10-25 18:37:38 +0200

    primer commit

---
+++ src/Hola.java
@@ -0,0 +1,15 @@
+/**
+ * fjalksdjfñajsf <strong>asdjflkañd f</strong> f asdf afa f
+ * @author Yo mismo
+ * @version 12
+ */
+public class Hola{
+	/**
+	 * dfkladjf lajflkd jsñlfkasdñfjañsdfjñksdafjñlsdaf
+	 * @param a recibe en este array de cadenas los parametros de ejecución
+	 */
+    public static void main(String[] a){
+		for (int i=1;i<=5;i++)
+			System.out.println("Antonio García 50"); // esto muestra por pantala un mensaje con hola mundo
+	}
+}
